fa409c919f023f60900e69594f48bd696e679df2
Allen Hughes <allen@brushycreekchurch.com>
2017-05-21 01:09:02 -0400

    removed unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :kids do
-    get 'register', on: :new
-    collection do
-      post 'confirm'
-    end
-  end
   
+  get '/signup', to: 'kids#register', as: 'register'
+  post 'confirm', to: 'kids#confirm', as: 'confirm'
   root to: 'kids#register'
 end
